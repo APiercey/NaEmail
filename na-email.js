68ed30c3b9bfea40fdf54eddbda69e49fe8c044f
Alexander Butt-Piercey <alex.bp@micrometrics.co>
2016-03-13 15:41:43 +0100

    replace inner text of email to email

--- na-email.js
+++ na-email.js
@@ -1,6 +1,14 @@
 var showEmail = function() {
-    
-}
+    naElements = document.getElementsByClassName('na-email');
+
+    for(var i = 0; i < naElements.length; i++) {
+        var e = naElements[i];
+
+        e.innerText = e.getAttribute('data-email-name')   + '@'
+                    + e.getAttribute('data-email-domain') + '.' 
+                    + e.getAttribute('data-email-top-level');
+    }
+};
 
 (window.addEventListener) ? window.addEventListener('load', showEmail, false) : window.attachEvent('onload', showEmail);
 
