9ef89d8de580e7a90ef9237cb615670db31615be
Alexander Butt-Piercey <alex.bp@micrometrics.co>
2016-03-13 16:03:32 +0100

    If an href attribute is present, add it

--- na-email.js
+++ na-email.js
@@ -1,14 +1,18 @@
 var showEmail = function() {
-    naElements = document.getElementsByClassName('na-email');
+    naElements = document.getElementsByClassName( 'na-email' );
 
-    for(var i = 0; i < naElements.length; i++) {
-        var e = naElements[i];
+    for( var i = 0; i < naElements.length; i++ ) {
+        var e       = naElements[i];
+        var replace = ( e.getAttribute( 'data-email-dont-replace' ) == undefined) ? true : false;
+        var email   = e.getAttribute( 'data-email-name' )   + '@'
+                    + e.getAttribute( 'data-email-domain' ) + '.' 
+                    + e.getAttribute( 'data-email-top-level' );
 
-        e.innerText = e.getAttribute('data-email-name')   + '@'
-                    + e.getAttribute('data-email-domain') + '.' 
-                    + e.getAttribute('data-email-top-level');
+        if( replace ) { e.innerText = email; }
+        if( e.getAttribute( 'href' ) ) { e.setAttribute( 'href', 'mailto:' + email ); }
     }
 };
 
-(window.addEventListener) ? window.addEventListener('load', showEmail, false) : window.attachEvent('onload', showEmail);
+( window.addEventListener ) ? window.addEventListener( 'load', showEmail, false )
+                            : window.attachEvent( 'onload', showEmail );
 
